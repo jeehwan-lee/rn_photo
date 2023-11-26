078e00c18483238afff067570057e10b1036170a
jeehwan <tjtojan@naver.com>
2023-11-27 00:10:08 +0900

    화면 추가

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HOME</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default HomeScreen;
